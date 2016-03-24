d7ff0eb69a6a118128c5e729e6998cf15e637261
andyfehrenbach <andy.fehrenbach@gmail.com>
2016-03-24 10:32:05 -0500

    set up sass file structure, moved dependencies to vendors folder

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.use(express.static('public/vendors'));
 app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function() {
-    console.log('Listening on port: ', app.get('port'));
+    console.log('Kicking ass and taking names on port ', app.get('port'));
 });
