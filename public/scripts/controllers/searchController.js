f5ea3211553a5b17b0d908c4abac20e4d0d67587
andyfehrenbach <andy.fehrenbach@gmail.com>
2016-03-26 13:11:34 -0500

    added carousel to search page.

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -5,8 +5,20 @@ myApp.controller('SearchController', ['$scope', '$http', 'DataFactory', function
 
 
     $scope.searchListings = function(){
-        console.log('YEEEAAAH!')
+        console.log('YEEEAAAH!');
     };
+
+    //begin elements for Angualar Carousel//
+
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  $scope.slides = [
+    {id: 0, image: '../../images/house-1.jpg'},
+    {id: 1, image: '../../images/house-2.jpg'}
+
+  ];
+  var currIndex = 0;
+
 
 
 }]);
