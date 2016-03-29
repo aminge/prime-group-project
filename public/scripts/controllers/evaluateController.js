a771bf777082818c0e90e22ecac7f127ea20c1c9
johndanielball <johndanielball86@gmail.com>
2016-03-28 23:23:33 -0500

    evaluate template update

--- public/scripts/controllers/evaluateController.js
+++ public/scripts/controllers/evaluateController.js
@@ -1,5 +1,4 @@
 myApp.controller('EvaluateController', ['$scope', 'DataFactory', function($scope, DataFactory) {
-  console.log('evaluate controller works');
 
   $scope.buyHoldForm = false;
   $scope.flipForm = true;
@@ -13,5 +12,12 @@ myApp.controller('EvaluateController', ['$scope', 'DataFactory', function($scope
   $scope.viewRent = function() {
     $scope.flipForm = false;
     $scope.buyHoldForm = true;
+  };
+
+  $scope.model = {
+    includeCloseHoldCosts: "1",
+    projectRehabPeriod: "2",
+    financingUsed: "1",
+    lenderAVRCostOfProject: "1"
   };
 }]);
