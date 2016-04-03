3d92a623af6624f21f868f5202db4ada89cf9d84
andyfehrenbach <andy.fehrenbach@gmail.com>
2016-04-02 22:25:59 -0500

    resources view wireframe built out using Flexbox.

--- public/scripts/controllers/resourcesController.js
+++ public/scripts/controllers/resourcesController.js
@@ -1,3 +1,6 @@
 myApp.controller('ResourcesController', ['$scope', 'DataFactory', function($scope, DataFactory){
   console.log('ResourcesController works' );
+
+  $scope.uploads = ['file #1', 'file #2', 'file #3', 'file #4', 'file #5', 'file#6'];
+
 }]);
