76c0b606d0e89d96689a450dcedf6b1cc76d3667
Alex Minge <aminge37@gmail.com>
2016-04-05 16:35:42 -0500

    Working on evaluate.html and evaluateController

--- public/scripts/controllers/evaluateController.js
+++ public/scripts/controllers/evaluateController.js
@@ -32,30 +32,50 @@ myApp.controller('EvaluateController', ['$scope', '$location', 'DataFactory', fu
       else if (str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9') {
         output += str[i];
       }
-      return parseInt(output);
     }
+    return parseInt(output);
   };
 
   $scope.justNumber2 = function(str) {
     return parseInt(str.split(/ /)[0].replace(/[^\d]/g, ''));
   };
 
-  $scope.purchasePrice = 50000;                    // 1
-  $scope.closingCosts = 1500;                      // 2
-  $scope.holdingCosts = 1500;                      // 3
-  $scope.includeCloseHoldCosts = true;             // 4
-  $scope.rehabBudget = 20000;                      // 5
-  $scope.projectRehabPeriod = 2;                   // 6
-  $scope.financingUsed = true;                     // 7
-  $scope.lenderAVRCostOfProject = 1;               // 8
-  $scope.maxPercentOfCostFinanced = 90;            // 9
-  $scope.originationDiscountPoints = 3;            // 10
-  $scope.otherClosingCosts = 0;                    // 11
-  $scope.pointsClosingCosts = 'backend';           // 12
+  $scope.purchasePrice = 50000;                     // 1
+  $scope.closingCosts = 1500;                       // 2
+  $scope.holdingCosts = 1500;                       // 3
+  $scope.includeCloseHoldCosts = true;              // 4
+  $scope.rehabBudget = 20000;                       // 5
+  $scope.projectRehabPeriod = 2;                    // 6
+  $scope.financingUsed = true;                      // 7
+  $scope.lenderAVRCostOfProject = 1;                // 8
+  $scope.maxPercentOfCostFinanced = 90;             // 9
+  $scope.originationDiscountPoints = 3;             // 10
+  $scope.otherClosingCosts = 0;                     // 11
+  $scope.pointsClosingCosts = 'backend';            // 12
+  $scope.interestRate = 10;                         // 13
+  $scope.interestPaymentDuringRehab = true;         // 14
+  $scope.splitBackendProfitsWithLender = true;      // 15
+  $scope.percentagePreTaxProfits = 50;              // 16
+  $scope.arvForFlip = 100000;                       // 17
+  $scope.monthsCompleteSaleAfterRehab = 2;          // 18
+  $scope.totalCapNeeded = $scope.purchasePrice + $scope.holdingCosts + $scope.closingCosts + $scope.rehabBudget;
+  $scope.maxDollarsFinanced = ($scope.purchasePrice + $scope.rehabBudget) * $scope.maxPercentOfCostFinanced / 100;
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
+  //$scope.
 
-  $scope.interestPaymentDuringRehab = 0;
+
   $scope.pointsClosingCosts = 1;
-  $scope.splitBackendProfitsWithLender = 1;
   $scope.monthsCompleteSaleAfterRehab = 2;
   $scope.monthsToRentAfterRehab = 2;
   $scope.refinancePerm = true;
@@ -64,7 +84,6 @@ myApp.controller('EvaluateController', ['$scope', '$location', 'DataFactory', fu
   $scope.doStuff = function() {
     console.log($scope.justNumber($scope.purchasePrice));
   }
-
 }]);
 
 String.prototype.splice = function(idx, rem, s) {
