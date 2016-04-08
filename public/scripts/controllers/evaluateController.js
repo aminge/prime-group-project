32bc079094f3cff0625b68041e3004cef01a787a
Alex Minge <aminge37@gmail.com>
2016-04-08 10:10:15 -0500

    Uncommented login restrictions

--- public/scripts/controllers/evaluateController.js
+++ public/scripts/controllers/evaluateController.js
@@ -1,11 +1,11 @@
 myApp.controller('EvaluateController', ['$scope', '$location', 'DataFactory', function($scope, $location, DataFactory) {
 
   // check to see if the user is logged in
-  //if (!DataFactory.factoryIsUserLoggedIn()) {
-  //  // redirect to login and display message letting user know they have to log in to see the content
-  //  DataFactory.factorySetReminderMessageToTrue();
-  //  $location.path('./views/templates/login.html');
-  //}
+  if (!DataFactory.factoryIsUserLoggedIn()) {
+    // redirect to login and display message letting user know they have to log in to see the content
+    DataFactory.factorySetReminderMessageToTrue();
+    $location.path('./views/templates/login.html');
+  }
 
   $scope.dataFactory = DataFactory;
 
--- public/scripts/controllers/mortgageController.js
+++ public/scripts/controllers/mortgageController.js
@@ -1,11 +1,11 @@
 myApp.controller('MortgageController', ['$scope', '$location', 'DataFactory', function($scope, $location, DataFactory) {
 
   // check to see if the user is logged in
-  //if (!DataFactory.factoryIsUserLoggedIn()) {
-  //  // redirect to login and display message letting user know they have to log in to see the content
-  //  DataFactory.factorySetReminderMessageToTrue();
-  //  $location.path('./views/templates/login.html');
-  //}
+  if (!DataFactory.factoryIsUserLoggedIn()) {
+    // redirect to login and display message letting user know they have to log in to see the content
+    DataFactory.factorySetReminderMessageToTrue();
+    $location.path('./views/templates/login.html');
+  }
   console.log('MortgageController works');
   $scope.dataFactory = DataFactory;
   $scope.results = false;
